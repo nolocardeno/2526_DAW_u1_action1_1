b40dc2ffdeb92aea2e328270a964bf240cb8c611
nolorubio23 <mcarsan165@g.educaand.es>
2025-10-09 08:32:20 +0200

    Primmer commit

---
+++ src/AdiosMundo.java
@@ -0,0 +1,12 @@
+/**
+ * Esta clase dice Adios mundo.
+ */
+public class AdiosMundo {
+
+    /**
+     * Este método hace un print de Adios mundo.
+     */
+    public static void main(String[] args) {
+        System.out.println("Adios mundo.");
+    }
+}
---
+++ src/HolaMundo.java
@@ -0,0 +1,13 @@
+
+/**
+ * Esta clase dice Hola mundo.
+ */
+public class HolaMundo {
+
+    /**
+     * Esta clase hace un print de Hola mundo.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo.");
+    }
+}
